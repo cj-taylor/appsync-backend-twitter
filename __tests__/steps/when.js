21288bf3bc4067eaff36ff89a2ef9a1b07439e65
CJ Taylor <cj.taylor@bluegrass.dev>
2021-07-17 14:51:18 -0400

    Add acceptance test for confirmUserSignup.

--- __tests__/steps/when.js
+++ __tests__/steps/when.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const AWS = require("aws-sdk");
 
 const we_invoke_confirmUserSignup = async (username, name, email) => {
   const handler = require("../../functions/confirm-user-signup").handler;
@@ -26,6 +27,47 @@ const we_invoke_confirmUserSignup = async (username, name, email) => {
   await handler(event, context);
 };
 
+const a_user_signs_up = async (password, name, email) => {
+  const cognito = new AWS.CognitoIdentityServiceProvider();
+
+  const userPoolId = process.env.COGNITO_USER_POOL_ID;
+  const clientId = process.env.WEB_COGNITO_USER_POOL_CLIENT_ID;
+
+  const signUpResponse = await cognito
+    .signUp({
+      ClientId: clientId,
+      Username: email,
+      Password: password,
+      UserAttributes: [
+        {
+          Name: "name",
+          Value: name,
+        },
+      ],
+    })
+    .promise();
+
+  const username = signUpResponse.UserSub;
+  console.log(`[${email}] - user has signed up [${username}]`);
+
+  // skips verification since we can't get the code via the email
+  // instead, admin function verifies for us to proceed using a fresh user
+  await cognito
+    .adminConfirmSignUp({
+      UserPoolId: userPoolId,
+      Username: username,
+    })
+    .promise();
+
+  console.log(`[${email}] - confirmed sign up`);
+  return {
+    username,
+    name,
+    email,
+  };
+};
+
 module.exports = {
   we_invoke_confirmUserSignup,
+  a_user_signs_up,
 };
