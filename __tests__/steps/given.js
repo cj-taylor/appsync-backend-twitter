90e29048233edb08a235396ff1c9b42ce9601ab1
CJ Taylor <cj.taylor@bluegrass.dev>
2021-07-17 17:07:27 -0400

    Add acceptance test for getMyProfile query.

--- __tests__/steps/given.js
+++ __tests__/steps/given.js
@@ -1,3 +1,5 @@
+require("dotenv").config;
+const AWS = require("aws-sdk");
 const chance = require("chance").Chance();
 const velocityUtil = require("amplify-appsync-simulator/lib/velocity/util");
 const a_random_user = () => {
@@ -33,7 +35,66 @@ const an_appsync_context = (identity, args) => {
   };
 };
 
+const an_authenticated_user = async () => {
+  const { name, email, password } = a_random_user();
+
+  const cognito = new AWS.CognitoIdentityServiceProvider();
+
+  const userPoolId = process.env.COGNITO_USER_POOL_ID;
+  const clientId = process.env.WEB_COGNITO_USER_POOL_CLIENT_ID;
+
+  const signUpResponse = await cognito
+    .signUp({
+      ClientId: clientId,
+      Username: email,
+      Password: password,
+      UserAttributes: [
+        {
+          Name: "name",
+          Value: name,
+        },
+      ],
+    })
+    .promise();
+
+  const username = signUpResponse.UserSub;
+  console.log(`[${email}] - user has signed up [${username}]`);
+
+  // skips verification since we can't get the code via the email
+  // instead, admin function verifies for us to proceed using a fresh user
+  await cognito
+    .adminConfirmSignUp({
+      UserPoolId: userPoolId,
+      Username: username,
+    })
+    .promise();
+
+  console.log(`[${email}] - confirmed sign up`);
+
+  const auth = await cognito
+    .initiateAuth({
+      AuthFlow: "USER_PASSWORD_AUTH",
+      ClientId: clientId,
+      AuthParameters: {
+        USERNAME: username,
+        PASSWORD: password,
+      },
+    })
+    .promise();
+
+  console.log(`[${email}] - signed in`);
+
+  return {
+    username,
+    name,
+    email,
+    idToken: auth.AuthenticationResult.IdToken,
+    accessToken: auth.AuthenticationResult.AccessToken,
+  };
+};
+
 module.exports = {
   a_random_user,
   an_appsync_context,
+  an_authenticated_user,
 };
