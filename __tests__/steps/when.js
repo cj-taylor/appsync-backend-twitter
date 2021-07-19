a902aa3c3a2020d8282b817ad7dc863f6f1150d1
CJ Taylor <cj.taylor@bluegrass.dev>
2021-07-19 09:28:04 -0400

    Implement Tweet.liked nested resolver.

--- __tests__/steps/when.js
+++ __tests__/steps/when.js
@@ -227,6 +227,7 @@ const a_user_calls_tweet = async (user, text) => {
       replies
       likes
       retweets
+      liked
     }
   }`;
   const variables = {
@@ -263,6 +264,7 @@ const a_user_calls_getTweets = async (user, userId, limit, nextToken) => {
           replies
           likes
           retweets
+          liked
         }
       }
     }
@@ -303,6 +305,7 @@ const a_user_calls_getMyTimeline = async (user, limit, nextToken) => {
           replies
           likes
           retweets
+          liked
         }
       }
     }
