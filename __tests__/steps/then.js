39de789c01dc0b4c5b2f3dcc994634a4bbee6be4
CJ Taylor <cj.taylor@bluegrass.dev>
2021-07-21 09:13:33 -0400

    Add integration tests for retweet mutation.

--- __tests__/steps/then.js
+++ __tests__/steps/then.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const _ = require("lodash");
 const AWS = require("aws-sdk");
 const http = require("axios");
 const fs = require("fs");
@@ -59,6 +60,49 @@ const tweet_exists_in_TweetsTable = async (id) => {
   return resp.Item;
 };
 
+const retweet_exists_in_TweetsTable = async (userId, tweetId) => {
+  const DynamoDB = new AWS.DynamoDB.DocumentClient();
+
+  console.log(
+    `looking for retweet of [${tweetId}] in table [${process.env.TWEETS_TABLE}]`
+  );
+  const resp = await DynamoDB.query({
+    TableName: process.env.TWEETS_TABLE,
+    IndexName: "retweetsByCreator",
+    KeyConditionExpression: "creator = :creator AND retweetOf = :tweetId",
+    ExpressionAttributeValues: {
+      ":creator": userId,
+      ":tweetId": tweetId,
+    },
+    Limit: 1,
+  }).promise();
+
+  const retweet = _.get(resp, "Items.0");
+
+  expect(retweet).toBeTruthy();
+
+  return retweet;
+};
+
+const retweet_exists_in_RetweetsTable = async (userId, tweetId) => {
+  const DynamoDB = new AWS.DynamoDB.DocumentClient();
+
+  console.log(
+    `looking for tweet [${tweetId}] for user [${userId}] in table [${process.env.RETWEETS_TABLE}]`
+  );
+  const resp = await DynamoDB.get({
+    TableName: process.env.RETWEETS_TABLE,
+    Key: {
+      userId,
+      tweetId,
+    },
+  }).promise();
+
+  expect(resp.Item).toBeTruthy();
+
+  return resp.Item;
+};
+
 const tweet_exists_in_TimelinesTable = async (userId, tweetId) => {
   const DynamoDB = new AWS.DynamoDB.DocumentClient();
 
@@ -76,6 +120,26 @@ const tweet_exists_in_TimelinesTable = async (userId, tweetId) => {
   expect(resp.Item).toBeTruthy();
 
   return resp.Item;
+};
+
+const there_are_N_tweets_in_TimelinesTable = async (userId, n) => {
+  const DynamoDB = new AWS.DynamoDB.DocumentClient();
+
+  console.log(
+    `looking for [${n}] tweets for user [${userId}] in table [${process.env.TIMELINES_TABLE}]`
+  );
+  const resp = await DynamoDB.query({
+    TableName: process.env.TIMELINES_TABLE,
+    KeyConditionExpression: "userId = :userId",
+    ExpressionAttributeValues: {
+      ":userId": userId,
+    },
+    ScanIndexForward: false,
+  }).promise();
+
+  expect(resp.Items).toHaveLength(n);
+
+  return resp.Items;
 };
 
 const tweetsCount_is_updated_in_UsersTable = async (id, newCount) => {
@@ -100,6 +164,9 @@ module.exports = {
   user_can_upload_image_to_url,
   user_can_download_image_from,
   tweet_exists_in_TweetsTable,
+  retweet_exists_in_TweetsTable,
+  retweet_exists_in_RetweetsTable,
   tweet_exists_in_TimelinesTable,
+  there_are_N_tweets_in_TimelinesTable,
   tweetsCount_is_updated_in_UsersTable,
 };
