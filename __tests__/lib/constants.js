caa83f8187194dcc60fffb7d31cba7e6d5a04357
CJ Taylor <cj-taylor@users.noreply.github.com>
2022-03-21 20:36:38 +0000

    Distribute tweets to followers

---
+++ __tests__/lib/constants.js
@@ -0,0 +1,14 @@
+const TweetTypes = {
+  TWEET: "Tweet",
+  RETWEET: "Retweet",
+  REPLY: "Reply",
+};
+
+const DynamoDB = {
+  MAX_BATCH_SIZE: 25,
+};
+
+module.exports = {
+  TweetTypes,
+  DynamoDB,
+};
